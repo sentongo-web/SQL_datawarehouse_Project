a880ad673674db473c5fed51ca2893df0c4192fa
Paul Sentongo <134306188+sentongo-web@users.noreply.github.com>
2026-08-02 18:51:54 +0300

    Create SQL script to confirm gold views
    Add SQL query to confirm existence of gold views.

--- scripts/gold_layer/views_confirm.sql
+++ scripts/gold_layer/views_confirm.sql
@@ -1,1 +1,14 @@
+/*=============================================================================
+CONFIRM THAT THE GOLD VIEWS EXIST
+=============================================================================*/
 
+SELECT
+    s.name AS schema_name,
+    v.name AS view_name,
+    v.create_date,
+    v.modify_date
+FROM sys.views AS v
+INNER JOIN sys.schemas AS s
+    ON v.schema_id = s.schema_id
+WHERE s.name = 'gold'
+ORDER BY v.name;
