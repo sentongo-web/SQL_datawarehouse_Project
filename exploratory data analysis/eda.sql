bd0c1a063831cf4d5c05397af4792afc997bb806
Paul Sentongo <134306188+sentongo-web@users.noreply.github.com>
2026-08-02 19:44:50 +0300

    Add exploratory queries for database analysis
    Added SQL queries to explore database tables and columns.

--- exploratory data analysis/eda.sql
+++ exploratory data analysis/eda.sql
@@ -1,1 +1,9 @@
+--Explaratory data analysis.
+-- Explore all objects in the database
 
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+-- explore all columns in the database
+-- check the schemas
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
